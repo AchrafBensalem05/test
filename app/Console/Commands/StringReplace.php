f3b9391ada754e4e6e6edd74c6a89190bf13c465
AchrafBensalem05 <ac.bensalem@esi-sba.dz>
2025-01-16 19:49:17 +0100

    added error handdling

--- app/Console/Commands/StringReplace.php
+++ app/Console/Commands/StringReplace.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use InvalidArgumentException;
 
 class StringReplace extends Command
 {
@@ -29,6 +30,18 @@ class StringReplace extends Command
     {
         $template = $this->argument('template');
         $arguments = $this->argument('arguments');
+
+        if (!preg_match_all('/\{(\d+)\}/', $template, $matches)) {
+            $this->error('Invalid template format. Placeholders should be like this {0}, {1}, etc.');
+            return Command::FAILURE;
+        }
+
+        $placeholderCount = count($matches[0]);
+        if ($placeholderCount > count($arguments)) {
+            $this->error('The number of arguments does not match.');
+            return Command::FAILURE;
+        }
+
         $result = $this->replacePlaceholders($template, $arguments);
         $this->info($result);
 
@@ -40,7 +53,11 @@ class StringReplace extends Command
     {
         return preg_replace_callback('/\{(\d+)\}/', function ($matches) use ($arguments) {
             $index = (int) $matches[1];
-            return $arguments[$index] ?? $matches[0];
+            if (!array_key_exists($index, $arguments)) {
+                $this->warn("Placeholder {$matches[0]} has no argument.");
+                return $matches[0]; 
+            }
+            return $arguments[$index];
         }, $template);
     }
 }
